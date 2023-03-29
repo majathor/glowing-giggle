cb9db7cbbebfae367b020f7eb212b42ef4129777
Max Thorniley <maxthorniley@gmail.com>
2023-03-29 17:36:34 +0800

    script.sh updated

--- gpu-assignment/ex1-gol-cuda/script.sh
+++ gpu-assignment/ex1-gol-cuda/script.sh
@@ -8,7 +8,16 @@
 
 module use /group/courses0100/software/nvhpc/modulefiles
 module load nvhpc/21.9
+module load gcc/9.2
 #srun nvcc -o myprogram myprogram.cu # for CUDA C++
 #srun nvfortran -o myprogram myprogram.cuf # for CUDE FORTRAN
 
-srun bin/game_of_life_c 5 50 10
+echo "Compiling code"
+srun --export=all -u -n 1 make clean
+srun --export=all -u -n 1 make
+
+echo "Running C"
+srun --export=all -u -n 1 bin/game_of_life_c 5 50 10
+
+#echo "Running CUDA"
+#srun --export=all -u -n 1 bin/game_of_life_cuda 5 50 10
